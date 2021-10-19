c084bb964cc4f0d354f9c87bf2ad388897d1fa76
KarlKo1 <karl-august.kongo@khk.ee>
2021-10-19 13:24:08 +0300

    As a user I can transfer funds to another account [Delivers #179733422]

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const mongoose = require('mongoose');
 const swaggerUi = require('swagger-ui-express')
 const yamljs = require('yamljs')
+const {processTransactions} = require("./middlewares");
 const swaggerDocument = yamljs.load('docs/swagger.yaml')
 
 require('dotenv').config()
@@ -13,14 +14,18 @@ app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 // Parse request body as JSON into req.body
 app.use(express.json())
 
-
 // Endpoints
+app.use('/transactions/b2b', function(req, res){
+    res.send({receiverName: "Jaan Tamm"})
+})
 app.use('/users', require('./routes/users'))
 app.use('/sessions', require('./routes/sessions'))
+app.use('/transactions', require('./routes/transactions'))
 
 mongoose.connect(process.env.MONGODB_URI, {}, function () {
     console.log('Connected to mongoDB')
 })
+processTransactions()
 
 app.listen(process.env.PORT, function () {
     console.log(`Listening on port ${process.env.PORT}`)
